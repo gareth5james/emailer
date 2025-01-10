6c2b032ad7030f6b9a17a8b03aabf80c48ea49f6
Gareth <gareth.james@force24.co.uk>
2025-01-10 16:23:22 +0000

    stuck

--- process.js
+++ process.js
@@ -1,62 +1,38 @@
 //tools
 
 const fs = require("fs");
-const cheerio = require("cheerio");
+//const cheerio = require("cheerio");
 const request = require("request");
 const JSZip = require("jszip");
 const path = require("path");
 
 //get emails
-const abandoned = fs.readFileSync(
-  "./source/Abandoned Browser/template.html",
-  "utf-8"
-);
+const abandoned = fs.readFileSync("./source/abandoned/template.html", "utf-8");
 const anniversary = fs.readFileSync(
-  "./source/AnniversaryBirthday Email/template.html",
+  "./source/anniversary/template.html",
   "utf-8"
 );
-const blog = fs.readFileSync("./source/Blog Email/template.html", "utf-8");
+const blog = fs.readFileSync("./source/blog/template.html", "utf-8");
 const confirmation1 = fs.readFileSync(
-  "./source/Confirmation Email 1/template.html",
+  "./source/confirmation1/template.html",
   "utf-8"
 );
 const confirmation2 = fs.readFileSync(
-  "./source/Confirmation Email 2/template.html",
+  "./source/confirmation2/template.html",
   "utf-8"
 );
-const event = fs.readFileSync("./source/Event Email/template.html", "utf-8");
-const nps1 = fs.readFileSync(
-  "./source/NPS Review Email 1/template.html",
-  "utf-8"
-);
-const nps2 = fs.readFileSync(
-  "./source/NPS Review Email 2/template.html",
-  "utf-8"
-);
-const nurture1 = fs.readFileSync(
-  "./source/Nurture Campaign 1/template.html",
-  "utf-8"
-);
-const nurture2 = fs.readFileSync(
-  "./source/Nurture Campaign 2/template.html",
-  "utf-8"
-);
-const prompt = fs.readFileSync(
-  "./source/Profile Prompt Email/template.html",
-  "utf-8"
-);
+const event = fs.readFileSync("./source/event/template.html", "utf-8");
+const nps1 = fs.readFileSync("./source/nps1/template.html", "utf-8");
+const nps2 = fs.readFileSync("./source/nps2/template.html", "utf-8");
+const nurture1 = fs.readFileSync("./source/nurture1/template.html", "utf-8");
+const nurture2 = fs.readFileSync("./source/nurture2/template.html", "utf-8");
+const prompt = fs.readFileSync("./source/prompt/template.html", "utf-8");
 const testimonial = fs.readFileSync(
-  "./source/Testimonial Email/template.html",
+  "./source/testimonial/template.html",
   "utf-8"
 );
-const webinar1 = fs.readFileSync(
-  "./source/Webinar Invite Email 1/template.html",
-  "utf-8"
-);
-const webinar2 = fs.readFileSync(
-  "./source/Webinar Invite Email 2/template.html",
-  "utf-8"
-);
+const webinar1 = fs.readFileSync("./source/webinar1/template.html", "utf-8");
+const webinar2 = fs.readFileSync("./source/webinar2/template.html", "utf-8");
 
 const emails = {
   abandoned,
@@ -81,6 +57,8 @@ const input = require("./input/input.json");
 
 //loop through and amend
 
+const zipper = {}
+
 for (email in emails) {
   let temp = emails[email];
 
@@ -97,5 +75,40 @@ for (email in emails) {
     });
   };
 
-  download(input.logoUrl, `./output/${email}/images/logo.jpg`, () => {});
+  download(input.logoUrl, `./source/${email}/images/logo.jpg`, () => {});
+
+  switch (email) {
+    case "nurture2":
+      temp = temp.replace("images/1712578675-4c24b22a.png", "images/logo.jpg");
+      break;
+    case "nurture1":
+      temp = temp.replace("images/1712576772-30444fb9.png", "images/logo.jpg");
+      break;
+    default:
+      temp = temp.replace("images/1712575426-3693fdeb.png", "images/logo.jpg");
+      break;
+  }
+
+  //zip
+
+  zipper[`${email}`] = new JSZip();
+
+  zipper[`${email}`].file("template.html", temp);
+
+  const images = fs.readdirSync(`./source/${email}/images`);
+
+  images.forEach((image) => {
+    const filePath = path.join(`./source/${email}/images`, image);
+    const content = fs.readFileSync(filePath);
+    zipper[`${email}`].file(`images/${image}`, content);
+  });
+
+  zipper[`${email}`]
+  .generateAsync({ type: "nodebuffer" })
+  .then((content) => {
+    fs.writeFileSync(`./${email}.zip`, content);
+  })
+  .catch((err) => {
+    console.error("Error creating zip file:", err);
+  });
 }
